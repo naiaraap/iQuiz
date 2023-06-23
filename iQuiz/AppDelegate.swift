a251963d229bc673b32e3a5e1d2a8947ea1509a6
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-23 16:04:27 -0300

    navigating to question view

--- iQuiz/AppDelegate.swift
+++ iQuiz/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    UIView.appearance().backgroundColor = UIColor(red: 82/255, green: 30/255, blue: 194/255, alpha: 1.0)
+    
     return true
   }
 
---
+++ iQuiz/ViewComponents/ButtonLayoutModel.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonLayoutModel.swift
+//  iQuiz
+//
+//  Created by Naiara de Almeida Pantuza on 23/06/23.
+//
+
+import UIKit
+
+class ButtonLayoutModel: UIButton {
+  
+    }
